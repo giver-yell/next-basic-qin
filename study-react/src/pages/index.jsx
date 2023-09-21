fc21e98234818820cded0e42a5b97eb316a9d410
Ryohei Fujihara <giver.yell@gmail.com>
2023-09-22 08:14:07 +0900

    ⚗️ clickEvent

--- study-react/src/pages/index.jsx
+++ study-react/src/pages/index.jsx
@@ -1,10 +1,22 @@
 import { Header } from '@/components/Header'
 import { Main } from '@/components/Main'
+import { useCallback } from 'react';
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href)
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index"/>
     </>
   )
