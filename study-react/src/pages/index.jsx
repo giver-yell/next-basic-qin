47e60efa8fba2b77606cc6421a385717307f4477
Ryohei Fujihara <giver.yell@gmail.com>
2023-09-23 12:25:14 +0900

    ✨ userEffect

--- study-react/src/pages/index.jsx
+++ study-react/src/pages/index.jsx
@@ -1,22 +1,30 @@
 import { Header } from '@/components/Header'
 import { Main } from '@/components/Main'
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export default function Home() {
-  const foo = 1;
+  // const foo = 1;
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href)
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  // const handleClick = useCallback((e) => {
+  //   console.log(e.target.href)
+  //   e.preventDefault();
+  //   alert(foo);
+  // }, []);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = 'lightblue';
+
+    return () => {
+      document.body.style.backgroundColor = '';
+    };
+  }, [])
 
   return (
     <>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      {/* <a href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </a> */}
       <Main page="index"/>
     </>
   )
