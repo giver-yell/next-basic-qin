ef6a68a14b72dbcfeb71ef7418ce4244d89e3551
giver.yell <giver.yell@gmail.com>
2023-10-02 07:59:34 +0700

    ✨ useMemo

--- study-react/src/pages/about.jsx
+++ study-react/src/pages/about.jsx
@@ -2,7 +2,7 @@ import { Header } from '@/components/Header'
 import { Main } from '@/components/Main'
 
 export default function About({
-    count,
+    doubleCount,
     isShow,
     handleClick,
     handleDisplay,
@@ -14,7 +14,7 @@ export default function About({
   return (
     <>
       <Header />
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={() => {handleDisplay()}}>{isShow ? "非表示" : "表示"}</button>
 
