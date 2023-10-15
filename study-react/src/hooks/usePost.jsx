6b0d642aaee60f69419dacdf6598e63cfc7d8206
giver.yell <giver.yell@gmail.com>
2023-10-15 17:12:58 +0900

    🎨 usePostの汎用性を高める

--- study-react/src/hooks/usePost.jsx
+++ study-react/src/hooks/usePost.jsx
@@ -1,14 +1,10 @@
 import { fetcher } from "@/utils/fetcher";
 
-const { useRouter } = require("next/router");
 const { default: useSWR } = require("swr");
 
-export const UsePost = () => {
-  const router = useRouter();
+export const UsePost = (id) => {
   const { data, error, isLoading } = useSWR(
-    router.query.id
-      ? `https://jsonplaceholder.typicode.com/posts/${router.query.id}`
-      : null,
+    id ? `https://jsonplaceholder.typicode.com/posts/${id}` : null,
     fetcher,
   );
 
