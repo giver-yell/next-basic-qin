a48127e5d3b71eeac2b381ec4fb49071c86836bd
giver.yell <giver.yell@gmail.com>
2023-10-15 18:02:50 +0900

    🔥 不要なcounterなど削除

--- study-react/src/pages/_app.jsx
+++ study-react/src/pages/_app.jsx
@@ -1,18 +1,11 @@
-import "@/styles/globals.css";
-import { useBgLightBlue } from "@/hooks/useBgColor";
-import { useCounter } from "@/hooks/useCounter";
-import { useInputArray } from "@/hooks/useInputArray";
+import "tailwindcss/tailwind.css";
 import { Layout } from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgLightBlue();
-
   return (
     <>
       <Layout>
-        <Component {...pageProps} {...counter} {...inputArray} />
+        <Component {...pageProps} />
       </Layout>
     </>
   );
