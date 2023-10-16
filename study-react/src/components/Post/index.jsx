dbc7ea4440879b735e741f2225277d3ac1b9da7f
giver.yell <giver.yell@gmail.com>
2023-10-16 09:43:05 +0900

    💄 全体的なデザイン

--- study-react/src/components/Post/index.jsx
+++ study-react/src/components/Post/index.jsx
@@ -2,6 +2,7 @@ import { CommentsByPostId } from "@/components/Comments/CommentsByPostId";
 import { UserById } from "@/components/User/UserById";
 
 import { UsePost } from "@/hooks/usePost";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 export const Post = () => {
@@ -17,11 +18,16 @@ export const Post = () => {
 
   return (
     <div>
-      <div>
+      <Head>
         <title>{data?.title}</title>
-        <p>{data?.body}</p>
-      </div>
+      </Head>
+
       <UserById id={data?.userId} />
+      <h1 className="text-3xl font-bold">{data?.title}</h1>
+      <p className="text-xl text-gray-900 mt-2">{data?.body}</p>
+      <div className="mt-2">
+        <h2 className="text-xl font-bold mt-10">コメント一覧</h2>
+      </div>
       <CommentsByPostId id={data?.id} />
     </div>
   );
