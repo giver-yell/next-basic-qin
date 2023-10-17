7529dfc746b5d3108168d2cc31123116faf00f26
giver.yell <giver.yell@gmail.com>
2023-10-18 08:39:22 +0900

    ✨ usersとuserのserver side rendering

--- study-react/src/pages/users/index.jsx
+++ study-react/src/pages/users/index.jsx
@@ -1,11 +1,29 @@
 import { Header } from "@/components/Header";
 import { UsersComponent } from "@/components/Users";
+import { fetcher } from "@/utils/fetcher";
+import { SWRConfig } from "swr";
 
-const Users = () => {
+export const getServerSideProps = async () => {
+  const USERS_API_URL = `https://jsonplaceholder.typicode.com/users`;
+  const usersData = await fetcher(USERS_API_URL);
+
+  return {
+    props: {
+      fallback: {
+        [USERS_API_URL]: usersData,
+      },
+    },
+  };
+};
+
+const Users = (props) => {
+  const { fallback } = props;
   return (
     <>
-      <Header />
-      <UsersComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <UsersComponent />
+      </SWRConfig>
     </>
   );
 };
