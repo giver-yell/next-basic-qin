255c4692c68bec658c48f29f8c667188c4ba0335
giver.yell <giver.yell@gmail.com>
2023-10-20 10:02:26 +0900

    ⚡️ コメントの個別ページをSG化

--- study-react/src/pages/comments/[id]/index.jsx
+++ study-react/src/pages/comments/[id]/index.jsx
@@ -1,11 +1,48 @@
 import { CommentComponent } from "@/components/Comment";
 import { Header } from "@/components/Header";
+import { fetcher } from "@/utils/fetcher";
+import { SWRConfig } from "swr";
 
-const CommentsId = () => {
+export const getStaticPaths = async () => {
+  const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`;
+  const commentsData = await fetcher(COMMENTS_API_URL);
+  const paths = commentsData.map((comment) => {
+    return {
+      params: {
+        id: comment.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const commentData = await fetcher(COMMENT_API_URL);
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentsId = (props) => {
+  const { fallback } = props;
+
   return (
     <div>
       <Header />
-      <CommentComponent />
+      <SWRConfig value={{ fallback }}>
+        <CommentComponent />
+      </SWRConfig>
     </div>
   );
 };
